0c4dce74307e541b7ede0bc45865634446854f19
Oliver Crossley <ollythecrossley@outlook.com>
2023-09-13 14:10:08 +0100

    prepares for hosting

--- location-api/src/App.jsx
+++ location-api/src/App.jsx
@@ -1,12 +1,54 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
+
   const [count, setCount] = useState(0)
+  const [myCrd, setMyCrd] = useState({})
+
+  function success(pos) {
+    const crd = pos.coords
+    setMyCrd(crd);
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+
+  function errors(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.permissions.query({name: "geolocation"}).then(function (result) {
+        console.log(result)
+        if (result.state === "granted") {
+          navigator.geolocation.getCurrentPosition(success, errors, options);
+        } else if (result.state === "prompt") {
+          navigator.geolocation.getCurrentPosition(success, errors, options);
+          console.log("We are in prompt mode!")
+        } else if (result.state === "denied") {
+          // do other things
+        }
+      })
+    } else {
+      console.log("Geolocation is not supported by this browser")
+    }
+    console.log(navigator)
+  }, []);
 
   return (
     <>
-
+      <p>Lat: {myCrd.latitude}</p>
+      <p>Long: {myCrd.longitude}</p>
+      <p>Acc: {myCrd.accuracy}</p>
     </>
   )
 }
